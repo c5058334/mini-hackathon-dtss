267c098b8d4255962927fa6ea6654d75efb8ed12
ardliath-shu <c5045540@hallam.shu.ac.uk>
2025-09-09 14:39:57 +0100

    Added row and column validation

--- js/main.js
+++ js/main.js
@@ -10,6 +10,30 @@
             input.className = `cell row-${row} col-${column}`;
             grid.appendChild(input);
 
+
+            input.onchange = (event) => {
+                
+                // Handle input change here
+                console.log(`Input changed at row ${row}, column ${column}:`, event.target.value);
+                
+
+                let validator = (selector) => {
+                    let theseElements = document.querySelectorAll(selector);
+                    let usedValues = [];
+                    theseElements.forEach((cell) => {
+                        if (cell.value && usedValues.includes(cell.value)) {
+                            theseElements.forEach(buddies => buddies.classList.add('invalid'));
+                        } else {
+                            usedValues.push(cell.value);
+                        }                    
+                    });
+                };
+
+                let theseElements = document.querySelectorAll('.invalid').forEach(invalids => invalids.classList.remove('invalid'));
+                
+                validator(`.row-${row}`);
+                validator(`.col-${column}`);
+            };
         }
     }
 })();
